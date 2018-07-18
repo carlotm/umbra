df0c2b078877cf47b10834d4990420bab8bc6bea
Ascani Carlo <carlo.ratm@gmail.com>
2018-07-18 21:01:35 +0200

    Add Gitlab CI

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    baseUrl: process.env.NODE_ENV === 'production'
+        ? '/umbra/'
+        : '/',
     productionSourceMap: false,
     configureWebpack: {
         entry: ['./src/main.js', './src/sass/main.scss'],
