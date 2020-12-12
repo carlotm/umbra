c5ceaa07d1b9495c5f1fc0608db34895d8f076a4
Ascani Carlo <carlo.ratm@gmail.com>
2020-12-12 10:39:16 +0100

    Fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/umbra/'
-        : '/',
+    publicPath: '/',
     productionSourceMap: false,
     configureWebpack: {
         entry: ['./src/main.js', './src/sass/main.scss'],
